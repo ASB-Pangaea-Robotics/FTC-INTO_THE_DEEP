8dfe43cb98e85b17123f814065716c6b58d8d902
SolBreaker1 <JBBallard2025@gmail.com>
2024-10-18 20:01:22 +0530

    CommandOpMode test made (will test tmrw trust)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/CommandOpModeTest.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.opmode.testing;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.common.Hardware;
+import org.firstinspires.ftc.teamcode.common.command.subsystemcommand.ExtensionCommand;
+import org.firstinspires.ftc.teamcode.common.subsystem.ExtensionSubsystem;
+
+@Config
+@TeleOp
+public class CommandOpModeTest extends CommandOpMode {
+
+    Hardware robot = Hardware.getInstance();
+    ExtensionSubsystem extension;
+
+    public static int targetPosition = 0;
+
+    GamepadEx gamepadEx;
+
+    @Override
+    public void initialize() {
+        gamepadEx.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
+                .whenPressed(() -> schedule(new ExtensionCommand(extension, targetPosition)));
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        extension.loop();
+        extension.write();
+    }
+
+
+}
