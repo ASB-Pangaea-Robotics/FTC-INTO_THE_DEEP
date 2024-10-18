997dbadff1fbcdb51df07964ebb7d9efd2a6c460
SolBreaker1 <JBBallard2025@gmail.com>
2024-10-18 20:00:40 +0530

    FTCDash installed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/ExtensionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/ExtensionTest.java
@@ -1,15 +1,20 @@
-package org.firstinspires.ftc.teamcode.opmode;
+package org.firstinspires.ftc.teamcode.opmode.testing;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandScheduler;
-import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.common.Hardware;
 import org.firstinspires.ftc.teamcode.common.command.subsystemcommand.ExtensionCommand;
 import org.firstinspires.ftc.teamcode.common.subsystem.ExtensionSubsystem;
 
+@Config
+@TeleOp
 public class ExtensionTest extends OpMode {
 
     Hardware robot = Hardware.getInstance();
@@ -23,10 +28,13 @@ public class ExtensionTest extends OpMode {
     public void init() {
         gamepadEx.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
                 .whenPressed(new ExtensionCommand(extension, targetPosition));
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
     public void loop() {
+        CommandScheduler.getInstance().run();
         extension.loop();
         extension.write();
     }
