dfce50d0e5e1ee8a99b180cff5d44865c411ab1f
SolBreaker1 <JBBallard2025@gmail.com>
2024-10-30 11:48:46 +0530

    Servo tuning :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/ServoTest.java
@@ -3,38 +3,40 @@ package org.firstinspires.ftc.teamcode.opmode.testing;
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.InstantCommand;
-import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.common.Globals;
 import org.firstinspires.ftc.teamcode.common.Hardware;
 import org.firstinspires.ftc.teamcode.common.subsystem.IntakeSubsystem;
 
 @Config
 @TeleOp(group = "Testing")
-public class ServoTuner extends OpMode {
+public class ServoTest extends OpMode {
 
     Hardware robot = Hardware.getInstance();
-    IntakeSubsystem intake = new IntakeSubsystem();
+    IntakeSubsystem intake;
 
     GamepadEx gamepadEx;
 
     @Override
     public void init() {
         robot.init(hardwareMap);
+        intake = new IntakeSubsystem();
 
         gamepadEx = new GamepadEx(gamepad1);
 
-        robot.intakeFourbarLeft.setPosition(0);
-        robot.intakeFourbarRight.setPosition(0);
+        CommandScheduler.getInstance().schedule(
+                new InstantCommand(() -> intake.setFourbar(Globals.FOURBAR_NUETRAL)));
 
-        gamepadEx.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
-                .whenPressed(new InstantCommand(() -> intake.setFourbar(robot.intakeFourbarRight.getPosition() + 0.01)));
-        gamepadEx.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER)
-                .whenPressed(new InstantCommand(() -> intake.setFourbar(robot.intakeFourbarRight.getPosition() - 0.01)));
+        gamepadEx.getGamepadButton(GamepadKeys.Button.DPAD_UP)
+                .whenPressed(new InstantCommand(() -> intake.setFourbar(Globals.FOURBAR_NUETRAL)));
+        gamepadEx.getGamepadButton(GamepadKeys.Button.DPAD_LEFT)
+                .whenPressed(new InstantCommand(() -> intake.setFourbar(Globals.FOURBAR_TRANSFER)));
+        gamepadEx.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT)
+                .whenPressed(new InstantCommand(() -> intake.setFourbar(Globals.FOURBAR_INTAKE)));
     }
 
     @Override
