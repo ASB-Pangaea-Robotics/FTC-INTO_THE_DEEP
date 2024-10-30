d3e9bea2771c5bd3f87ede07ec11eff0d4526fc6
SolBreaker1 <JBBallard2025@gmail.com>
2024-10-30 11:49:41 +0530

    random stuff idk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Globals.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class Globals {
+    public static double FOURBAR_NUETRAL = 0.32;
+    public static double FOURBAR_INTAKE = 0.045;
+    public static double FOURBAR_TRANSFER = 0.475;
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Hardware.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.common;
 
-import com.arcrobotics.ftclib.hardware.ServoEx;
-import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hardware {
 
@@ -19,11 +20,28 @@ public class Hardware {
 
     public MotorEx extensionMotor;
 
-    public ServoEx intakeFourbar;
+    public Servo intakeFourbarLeft;
+    public Servo intakeFourbarRight;
+
+    public CRServo intakeLeft;
+    public CRServo intakeRight;
+
+    public DigitalChannel intakeBreak;
+
 
     public void init(HardwareMap hardwareMap) {
-        extensionMotor = new MotorEx(hardwareMap, "leftFront");
+        extensionMotor = new MotorEx(hardwareMap, "extension");
+        extensionMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        extensionMotor.resetEncoder();
 
-        intakeFourbar = new SimpleServo(hardwareMap, "intakeFourbar", 0 , 360);
+        intakeFourbarLeft = hardwareMap.get(Servo.class, "intakeFourbarLeft");
+        intakeFourbarRight = hardwareMap.get(Servo.class, "intakeFourbarRight");
+        intakeFourbarLeft.setDirection(Servo.Direction.REVERSE);
+
+        intakeLeft = hardwareMap.get(CRServo.class, "intakeLeft");
+        intakeRight = hardwareMap.get(CRServo.class, "intakeRight");
+        intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intakeBreak = hardwareMap.get(DigitalChannel.class, "intakeBreak");
     }
 }
