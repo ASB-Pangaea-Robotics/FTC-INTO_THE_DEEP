cccb2f1a3491b045b80e83cbe2c6e65c8b2b13ab
SolBreaker1 <JBBallard2025@gmail.com>
2024-10-30 11:49:24 +0530

    extension working and tuned (maybe needs more)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/ExtensionSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/ExtensionSubsystem.java
@@ -1,42 +1,59 @@
 package org.firstinspires.ftc.teamcode.common.subsystem;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.common.Hardware;
 
+@Config
 public class ExtensionSubsystem extends SubsystemBase {
 
     Hardware robot = Hardware.getInstance();
 
     PIDController controller;
 
-    private static double kP = 0.0;
-    private static double kI = 0.0;
-    private static double kD = 0.0;
+    public static double kP = 0.035;
+    public static double kI = 0.0;
+    public static double kD = 0.0008;
 
-    private int target = 0;
+    private int current = 0;
+    public int target = 0;
+
+    private final int tolerance = 5;
 
     private double power = 0;
-    private double MAX_POWER = 0.5;
+    private double MAX_POWER = 0.3;
 
     public ExtensionSubsystem() {
         controller = new PIDController(kP, kI, kD);
     }
 
+    public void read() {
+        current = robot.extensionMotor.getCurrentPosition();
+    }
+
     public void loop() {
         controller.setPID(kP, kI, kD);
-        power = controller.calculate(robot.extensionMotor.getCurrentPosition(), target);
-
-        power = Range.clip(power, -MAX_POWER, MAX_POWER);
+        if (!atPosition()) {
+            power = controller.calculate(current, target);
+            power = Range.clip(power, -MAX_POWER, MAX_POWER);
+        } else
+            power = 0;
     }
 
     public void write() {
-        robot.extensionMotor.set(power);
+        if (!atPosition()) {
+            robot.extensionMotor.set(power);
+        }
     }
 
     public void setTarget(int target) {
         this.target = target;
+    }
+
+    public boolean atPosition() {
+        return Math.abs(target - current) < tolerance;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/ExtensionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/ExtensionTest.java
@@ -9,16 +9,19 @@ import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.common.Globals;
 import org.firstinspires.ftc.teamcode.common.Hardware;
 import org.firstinspires.ftc.teamcode.common.command.subsystemcommand.ExtensionCommand;
 import org.firstinspires.ftc.teamcode.common.subsystem.ExtensionSubsystem;
+import org.firstinspires.ftc.teamcode.common.subsystem.IntakeSubsystem;
 
 @Config
-@TeleOp
+@TeleOp(group = "Testing")
 public class ExtensionTest extends OpMode {
 
     Hardware robot = Hardware.getInstance();
     ExtensionSubsystem extension;
+    IntakeSubsystem intake;
 
     public static int targetPosition = 0;
 
@@ -28,16 +31,40 @@ public class ExtensionTest extends OpMode {
     public void init() {
         robot.init(hardwareMap);
 
-        gamepadEx.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
-                .whenPressed(new ExtensionCommand(extension, targetPosition));
+        extension = new ExtensionSubsystem();
+        intake = new IntakeSubsystem();
+
+        intake.setFourbar(Globals.FOURBAR_NUETRAL);
+
+        gamepadEx = new GamepadEx(gamepad1);
+
+//        gamepadEx.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
+//                .whenPressed(new ExtensionCommand(extension, targetPosition));
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
     public void loop() {
+
+        if (gamepad1.right_bumper) {
+            targetPosition=400;
+            extension.setTarget(targetPosition);
+        }
+
+        if (gamepad1.left_bumper) {
+            targetPosition=100;
+            extension.setTarget(targetPosition);
+        }
+
+        extension.read();
         CommandScheduler.getInstance().run();
         extension.loop();
         extension.write();
+
+        telemetry.addData("Target Position", targetPosition);
+        telemetry.addData("Current Position", robot.extensionMotor.getCurrentPosition());
+        telemetry.addData("At Position", extension.atPosition());
+        telemetry.update();
     }
 }
