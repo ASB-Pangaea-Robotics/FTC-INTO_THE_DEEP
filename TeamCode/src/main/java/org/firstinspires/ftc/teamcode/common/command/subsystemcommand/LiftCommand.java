4cea93be82dbae61986ddd1154f6d1975ec12406
SolBreaker1 <JBBallard2025@gmail.com>
2024-11-13 15:10:58 +0530

    lift command (will test tomorrow)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/command/subsystemcommand/LiftCommand.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.common.command.subsystemcommand;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+
+import org.firstinspires.ftc.teamcode.common.subsystem.OuttakeSubsystem;
+
+public class LiftCommand extends InstantCommand {
+    LiftCommand(OuttakeSubsystem outtake, int target) {
+        super(() -> outtake.setTarget(target));
+    }
+}
